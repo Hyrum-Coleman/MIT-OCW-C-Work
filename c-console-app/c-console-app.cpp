86722fb2bf48912ac25a8ada420b8713bc6d3f2a
Hyrum Coleman <hyrum.coleman@gmail.com>
2023-02-17 20:17:52 -0700

    statGen 3.2 HW1

--- c-console-app/c-console-app.cpp
+++ c-console-app/c-console-app.cpp
@@ -2,24 +2,49 @@
 //
 
 #include <iostream>
-#include <stdio.h>
+using namespace std;
+
+void statGen()
+{
+    int N;
+    int sum = 0;
+    int max = -1000000;
+    int min = 10000000;
+    int range = 0;
+    int mean = 0;
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        int userNum;
+        cin >> userNum;
+
+
+        sum += userNum;
+
+        if (userNum < min)
+        {
+            min = userNum;
+        }
+        if (userNum > max)
+        {
+            max = userNum;
+        }
+        
+    }
+    range = max - min;
+    mean = sum / N;
+
+    cout << "Mean: " << mean << "\n";
+    cout << "Max: " << max << "\n";
+    cout << "Min: " << min << "\n";
+    cout << "Range: " << range << "\n";
+}
+
 
 int main()
 {
-    int gaming = 256;
-
-    printf("We are gaming! %d", gaming);
-
+    statGen();
     return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
