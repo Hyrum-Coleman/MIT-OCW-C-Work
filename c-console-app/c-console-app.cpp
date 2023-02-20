602fd48aba4113501db4258df22885c1c97603aa
Hyrum Coleman <hyrum.coleman@gmail.com>
2023-02-19 23:04:19 -0700

    hw #2 3.3 error check

--- c-console-app/c-console-app.cpp
+++ c-console-app/c-console-app.cpp
@@ -1,13 +1,19 @@
 // c-console-app.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "factorialGoneWrong.h"
+#include <iostream>
 using namespace std;
 
+void doubleNumber(int *numPtr)
+{
+    *numPtr = *numPtr * 2;
+}
 
 int main()
 {
-    factorialCode();
+    int num = 35;
+    doubleNumber(&num);
+    cout << num;
     return 0;
 }
 
