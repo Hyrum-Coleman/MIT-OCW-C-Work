158955a3e9ce04dc227d55e5bf6c316d33c6178e
Hyrum <hyrum.coleman@gmail.com>
2023-03-17 23:53:43 -0600

    Finish HW4

--- c-console-app/main.cpp
+++ c-console-app/main.cpp
@@ -8,57 +8,52 @@ using namespace std;
 
 int main()
 {
-//    int intVal1;
-//    int intVal2;
-//    int intMin;
-//    cout << "Enter 2 integers to compare!";
-//    cin >> intVal1 >> intVal2;
-//
-//    double doubleVal1;
-//    double doubleVal2;
-//    double doubleMin;
-//    cout << "Enter 2 doubles to compare!";
-//    cin >> doubleVal1 >> doubleVal2;
-//
-//    char charVal1;
-//    char charVal2;
-//    char charMin;
-//
-//    cout << "Enter 2 characters to compare!";
-//    cin >> charVal1 >> charVal2;
-//
-//    float floatVal1;
-//    float floatVal2;
-//    float floatMin;
-//
-//    cout << "Enter 2 floats to compare";
-//    cin >> floatVal1 >> floatVal2;
-//
-//    intMin = MIN_MACRO(intVal1, intVal2);
-//    doubleMin = min_template(doubleVal1, doubleVal2);
-//    charMin = MIN_MACRO(charVal1, charVal2);
-//    floatMin = min_template(floatVal1, floatVal2);
-//
-//    cout << endl;
-//
-//    cout << "The minimum int is... " << intMin << endl;
-//    cout << "The minimum double is... " << doubleMin << endl;
-//    cout << "The minimum char is... " << charMin << endl;
-//    cout << "The minimum float is... " << floatMin << endl;
-//
-//    cout << endl << "BIG BREAK" << endl;
-//
-//    auto bLeftPoint = Point(3, 5);
-//    auto tRightPoint = Point (7, 9);
-//    auto rectPtr = new Rectangle(bLeftPoint, tRightPoint);
-//
-//    auto triPtr = dynamic_cast<Triangle*>(rectPtr);
-//
-//    cout << triPtr; // check it out, this is a null pointer!!
-//    delete triPtr; // ask greg if I actually need to delete triPtr (I assume so because nobody is complaining)
-//    delete rectPtr;
-//
-//    cout << endl << "BIG BREAK" << endl;
+    int int_val1;
+    int int_val2;
+    cout << "Enter 2 integers to compare!";
+    cin >> int_val1 >> int_val2;
+
+    double double_val1;
+    double double_val2;
+    cout << "Enter 2 doubles to compare!";
+    cin >> double_val1 >> double_val2;
+
+    char char_val1;
+    char char_val2;
+    cout << "Enter 2 characters to compare!";
+    cin >> char_val1 >> char_val2;
+
+    float float_val1;
+    float float_val2;
+
+    cout << "Enter 2 floats to compare";
+    cin >> float_val1 >> float_val2;
+
+    const int int_min = MIN_MACRO(int_val1, int_val2);
+    const double double_min = min_template(double_val1, double_val2);
+    const char char_min = MIN_MACRO(char_val1, char_val2);
+    const float float_min = min_template(float_val1, float_val2);
+
+    cout << endl;
+
+    cout << "The minimum int is... " << int_min << endl;
+    cout << "The minimum double is... " << double_min << endl;
+    cout << "The minimum char is... " << char_min << endl;
+    cout << "The minimum float is... " << float_min << endl;
+
+    cout << endl << "BIG BREAK" << endl;
+
+    auto b_left_point = Point(3, 5);
+    auto t_right_point = Point (7, 9);
+    const auto rect_ptr = new Rectangle(b_left_point, t_right_point);
+
+    const auto tri_ptr = dynamic_cast<Triangle*>(rect_ptr);
+
+    cout << tri_ptr; // check it out, this is a null pointer!!
+    delete tri_ptr; // ask greg if I actually need to delete triPtr (I assume so because nobody is complaining)
+    delete rect_ptr;
+
+    cout << endl << "BIG BREAK" << endl;
 
     Stack<int> my_stack;
     Stack<int> my_stack2;
