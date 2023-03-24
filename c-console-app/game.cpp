3e004ecadbf31f83606f8a62a8115a88883db669
Hyrum <hyrum.coleman@gmail.com>
2023-03-23 19:45:22 -0600

    consts and statics

--- c-console-app/game.cpp
+++ c-console-app/game.cpp
@@ -136,7 +136,8 @@ void Game::double_down()
     m_player_hand->print_hand();
 }
 
-void Game::hit(std::string& player_choice) {
+void Game::hit(std::string& player_choice) const
+{
 
     while (player_choice == "h")
     {
@@ -251,5 +252,3 @@ bool Game::get_continue_choice(std::string &continue_choice) {
             return false;
     }
 }
-
-
